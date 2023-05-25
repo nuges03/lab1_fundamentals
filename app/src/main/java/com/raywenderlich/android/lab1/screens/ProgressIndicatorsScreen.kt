e08902cc709a8874dc370af0f7273142dd7caeec
nuges03 <117296865+nuges03@users.noreply.github.com>
2023-05-25 21:58:15 +0300

    добавил ProgressBar !!

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
@@ -1,14 +1,24 @@
 package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import kotlinx.coroutines.delay
 
 @Composable
 fun ProgressIndicatorScreen() {
@@ -18,7 +28,23 @@ fun ProgressIndicatorScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        //TODO add your code here
+        var showProgressBar by remember { mutableStateOf(true) }
+
+        LaunchedEffect(Unit) {
+            delay(5000)
+            showProgressBar = false
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            if (showProgressBar) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.Center)
+                )
+            }
+        }
     }
 
     BackButtonHandler {
