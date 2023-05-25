a79236807a96e813e4b5de14c7430b27d5ac95db
nuges03 <117296865+nuges03@users.noreply.github.com>
2023-05-25 21:20:47 +0300

    добавил TextField !!

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -1,11 +1,33 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -25,7 +47,29 @@ fun TextFieldScreen() {
     }
 }
 
+
 @Composable
 fun MyTextField() {
-    //TODO add your code here
+
+    var field by remember {
+        mutableStateOf("")
+    }
+    Text(text = field,
+    modifier = Modifier.background(Color.Blue).padding(2.dp))
+
+    OutlinedTextField(
+        value = field,
+        onValueChange = {
+            field = it
+        },
+
+        placeholder = {
+            Text(text = "Вводи текст...")
+        },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next
+        ),
+        singleLine = true,
+        shape = RoundedCornerShape(11.dp)
+    )
 }
